0b3a0d4a5ce7e067466240871dcfaf4261139ed9
Jim Cipar <jim.cipar@redpanda.com>
2024-09-16 16:21:46 -0400

    datalake: arrow_to_iobuf interface
    This adds an arrow_to_iobuf interface that converts Arrow data to iobufs
    representing Parquet files that can be written to disk. There are two
    components:
    1. An implementation of arrow::io::OutputStream that collects data in
    iobufs
    2. A class that creates a parquet::io::FileWriter using that output
    stream and allows the caller to extract the generated iobufs.
    
    This allows us to separate the compute side of generating parquet, which
    still occurs in the Arrow library, from the file io, which can now be
    made seastar-friednly.

---
+++ src/v/datalake/parquet_writer.cc
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2024 Redpanda Data, Inc.
+ *
+ * Licensed as a Redpanda Enterprise file under the Redpanda Community
+ * License (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * https://github.com/redpanda-data/redpanda/blob/master/licenses/rcl.md
+ */
+#include "datalake/parquet_writer.h"
+
+#include "datalake/data_writer_interface.h"
+
+#include <arrow/array/array_base.h>
+#include <arrow/chunked_array.h>
+#include <arrow/io/file.h>
+#include <arrow/io/memory.h>
+#include <arrow/status.h>
+#include <arrow/table.h>
+#include <parquet/arrow/writer.h>
+#include <parquet/properties.h>
+
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+
+namespace datalake {
+class iobuf_output_stream : public arrow::io::OutputStream {
+public:
+    iobuf_output_stream() = default;
+
+    ~iobuf_output_stream() override = default;
+
+    //// OUTPUT STREAM METHODS ////
+
+    // Close the stream cleanly.
+    arrow::Status Close() override;
+
+    // Return the position in this stream
+    arrow::Result<int64_t> Tell() const override;
+
+    // Return whether the stream is closed
+    bool closed() const override;
+
+    arrow::Status Write(const void* data, int64_t nbytes) override;
+
+    // TODO: implement this to avoid copying data multiple times
+    // virtual Status Write(const std::shared_ptr<Buffer>& data);
+
+    //// METHODS SPECIFIC TO IOBUF OUTPUT STREAM ////
+    iobuf take_iobuf();
+
+private:
+    iobuf _current_iobuf;
+    bool _closed = false;
+    int64_t _position = 0;
+};
+
+arrow_to_iobuf::arrow_to_iobuf(const arrow::Schema& schema) {
+    // TODO: make the compression algorithm configurable.
+    std::shared_ptr<parquet::WriterProperties> writer_props
+      = parquet::WriterProperties::Builder()
+          .compression(arrow::Compression::SNAPPY)
+          ->build();
+
+    // Opt to store Arrow schema for easier reads back into Arrow
+    std::shared_ptr<parquet::ArrowWriterProperties> arrow_props
+      = parquet::ArrowWriterProperties::Builder().store_schema()->build();
+
+    _outfile = std::make_shared<iobuf_output_stream>();
+
+    auto writer_result = parquet::arrow::FileWriter::Open(
+      schema,
+      arrow::default_memory_pool(),
+      _outfile,
+      writer_props,
+      arrow_props);
+    if (!writer_result.ok()) {
+        throw std::runtime_error(fmt::format(
+          "Failed to create Arrow writer: {}", writer_result.status()));
+    }
+    _writer = std::move(writer_result.ValueUnsafe());
+}
+
+void arrow_to_iobuf::add_arrow_array(std::shared_ptr<arrow::Array> data) {
+    arrow::ArrayVector data_av = {data};
+
+    std::shared_ptr<arrow::ChunkedArray> chunked_data
+      = std::make_shared<arrow::ChunkedArray>(std::move(data_av));
+    auto table_result = arrow::Table::FromChunkedStructArray(chunked_data);
+    if (!table_result.ok()) {
+        throw std::runtime_error(fmt::format(
+          "Failed to create arrow table: {}", table_result.status()));
+    }
+    auto table = table_result.ValueUnsafe();
+    auto write_result = _writer->WriteTable(*table);
+    if (!write_result.ok()) {
+        throw std::runtime_error(fmt::format(
+          "Failed to write arrow table: {}", write_result.ToString()));
+    }
+}
+
+iobuf arrow_to_iobuf::take_iobuf() { return _outfile->take_iobuf(); }
+
+iobuf arrow_to_iobuf::close_and_take_iobuf() {
+    auto status = _writer->Close();
+    if (!status.ok()) {
+        throw std::runtime_error(
+          fmt::format("Failed to close FileWriter: {}", status.ToString()));
+    }
+    return take_iobuf();
+}
+
+arrow::Status iobuf_output_stream::Close() {
+    _closed = true;
+    return arrow::Status::OK();
+}
+
+arrow::Result<int64_t> iobuf_output_stream::Tell() const { return _position; };
+
+bool iobuf_output_stream::closed() const { return _closed; };
+
+arrow::Status iobuf_output_stream::Write(const void* data, int64_t nbytes) {
+    _current_iobuf.append(reinterpret_cast<const uint8_t*>(data), nbytes);
+    _position += nbytes;
+
+    return arrow::Status::OK();
+}
+iobuf iobuf_output_stream::take_iobuf() { return std::move(_current_iobuf); }
+
+} // namespace datalake
---
+++ src/v/datalake/parquet_writer.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2024 Redpanda Data, Inc.
+ *
+ * Licensed as a Redpanda Enterprise file under the Redpanda Community
+ * License (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * https://github.com/redpanda-data/redpanda/blob/master/licenses/rcl.md
+ */
+#pragma once
+
+#include "datalake/data_writer_interface.h"
+
+#include <arrow/io/file.h>
+#include <arrow/io/memory.h>
+#include <arrow/type.h>
+#include <parquet/arrow/writer.h>
+
+#include <filesystem>
+#include <utility>
+
+namespace arrow {
+class Array;
+}
+
+namespace datalake {
+
+class iobuf_output_stream;
+
+class arrow_to_iobuf {
+public:
+    explicit arrow_to_iobuf(const arrow::Schema& schema);
+
+    void add_arrow_array(std::shared_ptr<arrow::Array> data);
+    iobuf take_iobuf();
+    iobuf close_and_take_iobuf();
+
+private:
+    std::shared_ptr<iobuf_output_stream> _outfile;
+    std::unique_ptr<parquet::arrow::FileWriter> _writer;
+};
+
+} // namespace datalake
